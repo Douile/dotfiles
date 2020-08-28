a3e294379983f417a8fbd32e7876da7b9723c4b2
Douile <25043847+Douile@users.noreply.github.com>
2020-08-28 17:34:34 +0100

    [qtile] Update thermal widget

--- .config/qtile/widget/hwmon.py
+++ .config/qtile/widget/hwmon.py
@@ -1,3 +1,5 @@
+# -*- coding:utf-8 -*-
+
 import os
 import os.path
 
@@ -6,17 +8,20 @@ from libqtile.widget import base
 from libqtile.log_utils import logger
 from libqtile.utils import (
     UnixCommandNotFound,
-    UnixCommandRuntimeError,
     catch_exception_and_warn,
 )
 
 class ThermalHwmon(base.InLoopPollText):
     """Widget to display thermal information
+
+    To get a list of tag_sensors use `ls /sys/class/hwmon/hwmon**/temp*_input` in your terminal
+    The sensor tag name is the part before _input e.g. temp1
     """
     orientations = base.ORIENTATION_HORIZONTAL
     defaults = [
-        ('update_interval', 2, 'Update interval in seconds'),
+        ('update_interval', 1, 'Update interval in seconds'),
         ('tag_sensor', None, 'Tag of temperature sensor'),
+        ('threshold', 70 , 'If temp is above change to foreground_alert'),
         ('foreground_alert', 'ff0000', 'Foreground colour alert'),
         ('hwmon_dir', '/sys/class/hwmon', 'Path for hwmon devices'),
         ('format', '{temp}C', 'Formatting string'),
@@ -53,12 +58,12 @@ class ThermalHwmon(base.InLoopPollText):
 
     @catch_exception_and_warn(warning=UnixCommandNotFound, excepts=FileNotFoundError)
     def get_temp(self, sensor):
+        """Gets the temperature from given temp input (in degrees)"""
         temp = None
         with open(sensor, 'r') as sensor_input:
             temp = sensor_input.read()
         if temp is not None:
             return int(temp)/1000
-
 
     def poll(self):
         if self.sensor is None:
@@ -67,5 +72,10 @@ class ThermalHwmon(base.InLoopPollText):
         text = "N/A"
         if temp is not None:
             text = self.format.format_map({ 'temp': round(temp, 2) })
+
+            if temp > self.threshold:
+                self.layout.colour = self.foreground_alert
+            else:
+                self.layout.colour = self.foreground_normal
 
         return text
