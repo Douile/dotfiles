616751c3282a0bc8e3b3ea620ffd7a8aa303c945
Douile <25043847+Douile@users.noreply.github.com>
2020-09-17 23:34:37 +0100

    [qtile] Update licenses

--- .config/qtile/widget/net.py
+++ .config/qtile/widget/net.py
@@ -1,6 +1,7 @@
 # -*- coding:utf-8 -*-
 
 # Copyright (c) 2014 Rock Neurotiko
+# Copyright (c) 2020 Douile
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
@@ -19,6 +20,7 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
+
 from math import log
 from typing import Tuple
 
