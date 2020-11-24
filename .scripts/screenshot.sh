1a08124b38cb5297913ed67633c518b531745ce4
Douile <25043847+Douile@users.noreply.github.com>
2020-11-24 23:03:50 +0000

    Fix screenshot script

--- .scripts/screenshot.sh
+++ .scripts/screenshot.sh
@@ -1,9 +1,14 @@
 #!/bin/sh
 
 FILE="$HOME/Pictures/sc-$(date +%Y-%m-%d-%H-%M-%S).png";
-maim -s -u | tee "$FILE" | xclip -selection clipboard -t image/png && {
-  action=$(dunstify -a "maim" --action="show,Show in file explorer" -i "$FILE" "Screeshotted" "File saved as $FILE")
-  if test $action = 2; then
-    setsid pcmanfm $(dirname "$FILE")
-  fi
-}
+maim -s -u $FILE
+if test 0 -eq $?; then
+  cat "$FILE" | xclip -selection clipboard -t image/png && {
+    action=$(dunstify -a "maim" --action="show,Show in file explorer" -i "$FILE" "Screeshotted" "File saved as $FILE")
+    if test $action = 2; then
+      setsid pcmanfm $(dirname "$FILE")
+    fi
+  }
+else
+  dunstify -a "maim" --hints="string:bgcolor:#ff4444" "Screenshot" "Error taking screenshot"
+fi
